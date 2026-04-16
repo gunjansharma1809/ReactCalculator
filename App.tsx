f9a687ffa35cd598cca9953a17fd554556fcad97
gunjansharma1809 <gunjangnsharma@gmail.com>
2026-04-16 21:10:43 +0530

    Initial commit
    Generated by react-native@0.85.1

--- App.tsx
+++ App.tsx
@@ -1,45 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { FC } from 'react';
+import { Button, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+const App :FC= () =>{
+  return <View>
+    <Button onPress={()=>{
+      console.log("button is pressed")
+    }} title='This is button' color='red' ></Button>
     </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
