542be4c522d1c17a205fc8c8f290be8c399ce646
gunjansharma1809 <gunjangnsharma@gmail.com>
2026-04-20 10:48:46 +0530

    added styling

--- App.tsx
+++ App.tsx
@@ -1,12 +1,78 @@
 import { FC } from 'react';
-import { Button, View } from 'react-native';
+import {
+  Pressable,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  View,
+} from 'react-native';
 
-const App :FC= () =>{
-  return <View>
-    <Button onPress={()=>{
-      console.log("button is pressed")
-    }} title='This is button' color='red' ></Button>
+
+export default function App() {
+  return (
+    <View style={stylemyStyles.container}>
+      <Pressable
+        style={stylemyStyles.buttonStyle}
+        onPress={() => console.log('black button pressed')}
+      >
+        <Text
+          style={stylemyStyles.text}
+        >
+        0
+        </Text>
+      </Pressable>
+      <Pressable
+        style={stylemyStyles.buttonStyle}
+        onPress={() => console.log('black button pressed')}
+      >
+        <Text
+          style={stylemyStyles.text}
+        >
+        1
+        </Text>
+      </Pressable>
+      <Pressable
+        style={stylemyStyles.buttonStyle}
+        onPress={() => console.log('black button pressed')}
+      >
+        <Text
+          style={stylemyStyles.text}
+        >
+        2
+        </Text>
+      </Pressable>
+      <Pressable
+        style={stylemyStyles.buttonStyle}
+        onPress={() => console.log('black button pressed')}
+      >
+        <Text
+          style={stylemyStyles.text}
+        >
+        3
+        </Text>
+      </Pressable>
     </View>
+  );
 }
 
-export default App
+const stylemyStyles = StyleSheet.create({
+  container:{padding: 50,flexDirection:'column'},
+  text:{ 
+    color: 'white', 
+    fontSize: 50,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  buttonStyle:{
+    backgroundColor: 'black',
+    width:70,
+    height:80,
+    borderRadius:40,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent:'center'
+      }
+   
+})
